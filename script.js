06fdd25f0ab50c7ab4594a9a7ec6d6808d1a6c39
pares-seux <paresseux.992@gmail.com>
2022-02-21 16:47:58 +0400

    Additional task

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.fontSize = fontSize;
 };
 
-DomElement.prototype.createNewElement = function (value) {
+DomElement.prototype.createNewElement = function (value, position) {
   let newElement;
   const attributeName = this.selector.slice(1);
   switch (this.selector[0]) {
@@ -34,11 +34,31 @@ DomElement.prototype.createNewElement = function (value) {
     this.bg +
     "; font-size: " +
     this.fontSize +
-    "px;";
+    "px; position: " +
+    position +
+    ";";
   return newElement;
 };
 
-const div = new DomElement("#header", 100, 200, "green", 15);
-const p = new DomElement(".footer", 400, 100, "red", 11);
-body.append(div.createNewElement("inside text"));
-body.append(p.createNewElement("kukusiki"));
+const div = new DomElement("#square", 100, 100, "green");
+const square = div.createNewElement("", "absolute");
+document.addEventListener("DOMContentLoaded", function (event) {
+  body.append(square);
+});
+body.addEventListener("keydown", function (event) {
+  if (
+    event.code != "ArrowRight" &&
+    event.code != "ArrowLeft" &&
+    event.code != "ArrowUp" &&
+    event.code != "ArrowDown"
+  )
+    return;
+  let x = parseInt(square.style.top),
+    y = parseInt(square.style.left);
+  if (isNaN(x)) x = 0;
+  if (isNaN(y)) y = 0;
+  if (event.code === "ArrowRight") square.style.left = `${y + 10}px`;
+  if (event.code === "ArrowLeft") square.style.left = `${y - 10}px`;
+  if (event.code === "ArrowUp") square.style.top = `${x - 10}px`;
+  if (event.code === "ArrowDown") square.style.top = `${x + 10}px`;
+});
