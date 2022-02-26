e9ea98f570ce7788cb032d4aecc02b9bb5d96520
pares-seux <paresseux.992@gmail.com>
2022-02-26 18:11:46 +0400

    added loop

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 };
 
 DomElement.prototype.createNewElement = function (value, position) {
-  let newElement;
+  let newElement,
+    cssString = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontSize}px; margin: 0;`;
   const attributeName = this.selector.slice(1);
   switch (this.selector[0]) {
     case ".":
@@ -25,23 +26,16 @@ DomElement.prototype.createNewElement = function (value, position) {
       return;
   }
   newElement.textContent = value;
-  newElement.style.cssText =
-    "height: " +
-    this.height +
-    "px; width: " +
-    this.width +
-    "px; background-color: " +
-    this.bg +
-    "; font-size: " +
-    this.fontSize +
-    "px; position: " +
-    position +
-    ";";
+  if (position === "absolute") {
+    cssString += `position: absolute; top: 0; left: 0;`;
+  }
+  newElement.style.cssText = cssString;
   return newElement;
 };
 
 const div = new DomElement("#square", 100, 100, "green");
 const square = div.createNewElement("", "absolute");
+
 document.addEventListener("DOMContentLoaded", function (event) {
   body.append(square);
 });
@@ -57,8 +51,23 @@ body.addEventListener("keydown", function (event) {
     y = parseInt(square.style.left);
   if (isNaN(x)) x = 0;
   if (isNaN(y)) y = 0;
-  if (event.code === "ArrowRight") square.style.left = `${y + 10}px`;
-  if (event.code === "ArrowLeft") square.style.left = `${y - 10}px`;
-  if (event.code === "ArrowUp") square.style.top = `${x - 10}px`;
-  if (event.code === "ArrowDown") square.style.top = `${x + 10}px`;
+  if (event.code === "ArrowRight") y += 10; //square.style.left = `${y + 10}px`;
+  if (event.code === "ArrowLeft") y -= 10; //square.style.left = `${y - 10}px`;
+  if (event.code === "ArrowUp") x -= 10; //square.style.top = `${x - 10}px`;
+  if (event.code === "ArrowDown") x += 10; //square.style.top = `${x + 10}px`;
+  console.log(window.innerWidth);
+  y =
+    y > window.innerWidth - parseInt(square.style.width)
+      ? 0
+      : y < parseInt(square.style.width) * -1
+      ? window.innerWidth - parseInt(square.style.width)
+      : y;
+  x =
+    x > window.innerHeight - parseInt(square.style.height)
+      ? 0
+      : x < parseInt(square.style.height) * -1
+      ? window.innerHeight - parseInt(square.style.height)
+      : x;
+  square.style.left = `${y}px`;
+  square.style.top = `${x}px`;
 });
